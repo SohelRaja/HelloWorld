d3848e70e33c1909564b999e052fe7368777b9c8
kartik2797 <43718146+kartik2797@users.noreply.github.com>
2019-10-08 10:49:00 +0530

    Create customer_class.cpp
    Created a simple bank customer class

---
+++ customer_class.cpp
@@ -0,0 +1,11 @@
+class Customer{
+  public:
+    string cust_name;
+    int cust_id;
+    long long cust_balance;
+  Customer(){
+  cust_name="Bank_default";
+  cust_id=123456;
+  cust_balance=0;
+  }
+  };
